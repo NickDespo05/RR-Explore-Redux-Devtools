7d4b054492068907196f825a42d5f553b91964c3
Nick D'Esposito <Nicholas D'Esposito>
2022-08-19 14:03:28 -0400

    done

--- src/store.js
+++ src/store.js
@@ -3,6 +3,8 @@ import counterReducer from './features/counterSlice'
 import todoReducer from './features/todoSlice'
 
 export const store = configureStore({
+   
+    
     reducer: {
         counter: counterReducer,
         todos: todoReducer
